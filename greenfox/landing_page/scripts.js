fee2c8fc5ec85abb05745949554334f8c58bcdb5
palffyzoltan <87267570+palffyzoltan@users.noreply.github.com>
2021-07-19 11:52:40 +0200

    Landing Page 4.0

---
+++ greenfox/landing_page/scripts.js
@@ -0,0 +1,65 @@
+let kepadatok = [
+    {
+        foto: 'images/1.jpg',
+        felirat: 'Na elindulunk?',
+        leiras: 'Az első lépések nehezek. De megoldjuk!'
+    },
+    {
+        foto: 'images/2.jpg',
+        felirat: 'Többen is tudunk evezni...',
+        leiras: 'Jó lenne eldönteni, merre van az előre. Vitassuk meg!'
+    },
+    {
+        foto: 'images/3.jpg',
+        felirat: 'Nagy a szárazság',
+        leiras: 'Hol van a víz? Gyerünk keressük meg!'
+    },
+    {
+        foto: 'images/4.jpg',
+        felirat: 'Kalandorok kíméljenek!',
+        leiras: 'Megtaláltátok a vizet? Na érjetek utól!'
+    },
+]
+
+let fotobetolt = (AktFoto) => {
+    $('#kep').attr('src', kepadatok[AktFoto].foto);
+    $('#fotofelirat').text(kepadatok[AktFoto].felirat);
+    $('#fotoleiras').text(kepadatok[AktFoto].leiras);
+    for (let index = 0; index < kepadatok.length; index++) {
+        $(`#index${index}`).css("border", "1px solid green"); 
+    }
+    $(`#index${AktFoto}`).css("border", "2px solid red");
+}
+
+AktFoto=0;
+fotobetolt(AktFoto)
+
+$('#jobbgomb').click(() => {
+    AktFoto++;
+    if (AktFoto===4) {AktFoto=0}
+    fotobetolt(AktFoto);
+  })
+
+  $('#balgomb').click(() => {
+    AktFoto--;
+    if (AktFoto===-1) {AktFoto=3}
+    fotobetolt(AktFoto);
+  })
+
+function IndexMegjelenit(kep,sorsz){
+    $('#indexdoboz').append(`<div class="indexdobozelem"><img src="${kep.foto}" class="index" data-number="${sorsz}" id="index${sorsz}"><p class="indexszoveg">${kep.felirat}</p></div>`)
+}
+
+/*IndexMegjelenit(2);*/
+/*kepadatok.forEach(IndexMegjelenit)*/
+
+for (let index = 0; index < kepadatok.length; index++) {
+    IndexMegjelenit(kepadatok[index],index);        
+}
+
+$('.index').click((event) => {
+    let aktindex = $(event.target).attr('data-number');
+    let aktindexszam = parseInt(aktindex);
+    fotobetolt(aktindexszam);
+})
+
